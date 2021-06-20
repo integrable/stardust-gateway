4f7623a19a1e76bcb600230a76decd93367b0bd8
Kamil Szewc <kamil.szewc@gmail.com>
2021-06-20 16:30:28 +0200

    Added deploy.yml

--- src/main/java/eu/integrable/numericalmechanicsgateway/NumericalMechanicsGatewayApplication.java
+++ src/main/java/eu/integrable/numericalmechanicsgateway/NumericalMechanicsGatewayApplication.java
@@ -2,7 +2,12 @@ package eu.integrable.numericalmechanicsgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NumericalMechanicsGatewayApplication {
 
@@ -10,4 +15,8 @@ public class NumericalMechanicsGatewayApplication {
         SpringApplication.run(NumericalMechanicsGatewayApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes().build();
+    }
 }
