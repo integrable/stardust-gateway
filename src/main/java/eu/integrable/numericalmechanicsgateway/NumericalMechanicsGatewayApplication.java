95d1c7cd72708fcc62349e8495b723e69d653c03
Kamil Szewc <kamil.szewc@gmail.com>
2021-06-20 14:47:41 +0200

    Initial commit

---
+++ src/main/java/eu/integrable/numericalmechanicsgateway/NumericalMechanicsGatewayApplication.java
@@ -0,0 +1,13 @@
+package eu.integrable.numericalmechanicsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumericalMechanicsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumericalMechanicsGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/integrable/numericalmechanicsgateway/NumericalMechanicsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.integrable.numericalmechanicsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumericalMechanicsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
